ce1e145ed49861d815ad4f44e2f569a8a32319c6
JONATHAN FREIRE <jonathan.freire@devsu.com>
2022-12-08 23:56:54 -0500

    Removed default values, added bootstrap cdn

---
+++ src/commons/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">SISTEMA DE FACTURACION</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
+  )
+}
+
+export default Navbar;
